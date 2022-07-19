e947c24eedbb7c4e39b6b1910af2f561d2b94533
ediazcorti <ediazcorti@gmail.com>
2022-07-19 13:25:41 -0300

    VersionRoutingEnAppJS 19_7 13.25hs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import {BrowserRouter, Routes , Route} from 'react-router-dom';
+import Login from './pages/Login';
+
 
 //test1
 
 function App() {
   return (
-    <Layout className="Layout">
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Bienvenido al registro / Login
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </Layout>
+
+<BrowserRouter>
+<Layout>
+<Routes> 
+<Route path="/" element={Login} />
+
+</Routes>
+</Layout>
+</BrowserRouter> 
+
   );
 }
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Routes from './routes/Routes';
-import Applicacion from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routes/>
+    <App/>
   </React.StrictMode>
 );
 
