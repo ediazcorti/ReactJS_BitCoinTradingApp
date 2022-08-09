3bf46bbc689e39f46c0ad4f858f9bd2002ffdabf
Emanuel Diaz Corti <ediazcorti@gmail.com>
2022-08-09 00:07:06 -0300

    IA Sugerencias Added

--- src/components/Pages/Dashboard/IASugerencias/CardMoneda.jsx
+++ src/components/Pages/Dashboard/IASugerencias/CardMoneda.jsx
@@ -1,26 +1,41 @@
 import React from "react";
 
-const CardMoneda = ({monedaId, tipoUltimaOperacion, valorUltTransacc, valorActualMoneda, recomendacion }) => { 
+const CardMoneda = ({monedas, monedaId, tipoUltimaOperacion, valorUltTransacc, valorActualMoneda, recomendacion }) => { 
 
-
+    const EncontrarNombreMoneda = (id) => {
+        let moneda = monedas.listaMonedas.find(item => item.id === id)
+        return moneda ? moneda.nombre : 0
+      }
 
     return  (
-    <>
-    <h1>CARD</h1>
-    <div className="Card">
-    <p>{monedaId}</p>
-    <p>{tipoUltimaOperacion}</p>
-    <p>{valorUltTransacc}</p>
-    <p>{valorActualMoneda}</p>
-    <p>{recomendacion}</p>
+        <div className="card">
+     
+    
+   
+    <h4>Nombre de moneda:</h4>
+    <h3 class="text-primary">{EncontrarNombreMoneda(monedaId)}</h3>
+     {/* <h5 class="card-title">ID Moneda: </h5>
+     <p class="card-text">{monedaId} </p> */}
 
+    <h6>Tipo de Última Operacion:</h6>
+    {tipoUltimaOperacion == 1 ? "Compra" : "Venta"}
+    <br />
+    <h6>Valor de la última transacción:</h6>
+    <p class="card-text"> {valorUltTransacc}</p>
+    <h6>Valor actual de esta Moneda:</h6>
+    <p class="card-text">{valorActualMoneda}</p>
+    <br />
+    <h6>La recomendación es:</h6>
+    <p class="card-text">{recomendacion}</p>
+  <br />
+    
+   
 
-
-
-    </div>
+ 
+</div>
 
     
-    </>
+  
     )
 }
 
--- src/components/Pages/Dashboard/IASugerencias/IASugerencias.jsx
+++ src/components/Pages/Dashboard/IASugerencias/IASugerencias.jsx
@@ -1,110 +1,82 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux/es/exports";
-import CardMoneda from "./CardMoneda";
-import { useEffect } from "react";
-
-
-const IASugerencias = ({ transacciones, obtenerMonedas, monedas, apiKey, ObtenerTransacciones, user }) => {
-    const dispatch = useDispatch()
-
-
-    let listaTransacciones = []
-    console.log("MONEDAS DENTRO DE IA SUGERENCIAS ES ----------------------", monedas)
-    console.log("TRANSACCIONES DENTRO DE IA SUGERENCIAS ES", transacciones)
-    console.log("LA APIKEY ES", apiKey)
-    console.log("EL USUARIO ACTUAL ES", user)
-    console.log("FIN DE IASUGERENCIAS--------------------------------------------------")
-    let ultimaTransaccId = 0
-    const mapMonedasUltimaTrans = [] // { monedaId: id, ultTrans: {} }
-
-    const buscarMonedaUltTransac = (id) => {
-        return mapMonedasUltimaTrans.find((item) =>  item.monedaId == id)
-    }
-
-
- const construirSugerencias = () => { 
-    transacciones.map((transaction) => {
-        const monedaBuscada = buscarMonedaUltTransac(transaction.moneda)
-
-        if (monedaBuscada) {
-
-            monedaBuscada.ultTrans = transaction
-
-        } else {
-            mapMonedasUltimaTrans.push(
-                {
-                    monedaId: transaction.moneda,
-                    ultTrans: transaction
-
-                }
-
-            )
+import React, { useEffect, useState } from 'react'
+import CardMoneda from './CardMoneda'
+const IASugerencias = ({
+  transacciones,
+  monedas
+}) => {
+  const [mapMonedasUltimaTrans, setMapMonedasUltimaTrans] = useState([])
+  const buscarMonedaUltTransac = (id, partialArray) => {
+    return partialArray.find(item => item.monedaId == id)
+  }
+  const _construirSugerencias = () => {
+    const partialArray = []
+    transacciones.map(transaction => {
+      const monedaBuscada = buscarMonedaUltTransac(
+        transaction.moneda,
+        partialArray
+      )
+      if (monedaBuscada) {
+        monedaBuscada.ultTrans = transaction
+      } else {
+        if (transaction.moneda !== 0) {
+          partialArray.push({
+            monedaId: transaction.moneda,
+            ultTrans: transaction
+          })
         }
+      }
     })
-
- }
-
- useEffect(() => {
-    construirSugerencias()
-    console.log("1 MAPA MONEDAS ULTIMA TRANSACCION EN IA SUGERENCISA --------------", mapMonedasUltimaTrans)
-
-}, [])
-
-
-    useEffect(() => {
-        construirSugerencias()
-        console.log("2 MAPA MONEDAS ULTIMA TRANSACCION EN IA SUGERENCISA --------------", mapMonedasUltimaTrans)
-    }, [transacciones, mapMonedasUltimaTrans])
-
-
-
-
-    // FUNCION DE RECOMENDACION:
-    const recomendacion = ({ valorActualMoneda, valorUltTransacc, tipoUltimaOperacion }) => {
-        let recom = ""
-
-        // SI FUE COMPRA Y EL VALOR DE LA MONEDA ES MAYOR?::::
-        if (valorActualMoneda > valorUltTransacc && tipoUltimaOperacion == 1) {
-            recom = "Vender"
-            // SI FUE COMPRA Y EL VALOR ES MENOR
-        } else if (valorActualMoneda < valorUltTransacc && tipoUltimaOperacion == 1) {
-            recom = "Comprar"
-
-            // Si fue una VENTA y el Valor BAJÓ
-        } else if (valorActualMoneda < valorUltTransacc && tipoUltimaOperacion != 1) {
-
-            recom = "Comprar"
-        } else {
-            recom = "Vender"
-        }
-
-        return recom
-
+    setMapMonedasUltimaTrans(partialArray)
+  }
+  useEffect(() => {
+    _construirSugerencias()
+  }, [transacciones])
+  // FUNCION DE RECOMENDACION:
+  const recomendacion = (
+    valorActualMoneda,
+    valorUltTransacc,
+    tipoUltimaOperacion
+  ) => {
+     let recom = 'Default'
+    // SI FUE COMPRA Y EL VALOR DE LA MONEDA ES MAYOR?::::
+    if (valorActualMoneda > valorUltTransacc && tipoUltimaOperacion == 1) {
+      recom = 'Vender'
+      // SI FUE COMPRA Y EL VALOR ES MENOR
+    } else if (
+      valorActualMoneda < valorUltTransacc &&
+      tipoUltimaOperacion == 1
+    ) {
+      recom = 'Comprar'
+      // Si fue una VENTA y el Valor BAJÓ
+    } else if (
+      valorActualMoneda < valorUltTransacc &&
+      tipoUltimaOperacion !== 1
+    ) {
+       recom = 'Comprar'
+    } else {
+      recom = 'Vender'
     }
-
-    const EncontrarValorActualMoneda = (id) => {
-        let moneda = monedas.listaMonedas.find((item) =>  item.monedaId == id )
-        return moneda.cotizacion
-
-    }
-
-    return (
-        <>
-            <div id="contenedor" className="container">
-                <h1>Contenedor </h1>
-
-{  mapMonedasUltimaTrans.map(( obj ) => <CardMoneda monedaId={obj.monedaId} tipoUltimaOperacion={obj.ultTrans.tipo_operacion}
-                    valorUltTransacc={obj.ultTrans.valor_actual} valorActualMoneda={EncontrarValorActualMoneda(obj.monedaId)} 
-                />) 
-                   
-                    }
-
-            </div>
-        </>
-
-    )
-
-
+    return recom
+  }
+  const EncontrarValorActualMoneda = id => {
+    let moneda = monedas.listaMonedas.find(item => item.id === id)
+    return moneda ? moneda.cotizacion : 0
+  }
+  return (
+    <>
+      <div id='contenedor' className='container'>
+        <h1>Contenedor </h1>
+        {mapMonedasUltimaTrans.map(obj => (
+          <CardMoneda monedas={monedas}
+            monedaId={obj.monedaId}
+            tipoUltimaOperacion={obj.ultTrans.tipo_operacion}
+            valorUltTransacc={obj.ultTrans.valor_actual}
+            valorActualMoneda={EncontrarValorActualMoneda(obj.monedaId)}
+            recomendacion={recomendacion(EncontrarValorActualMoneda(obj.monedaId), obj.ultTrans.valor_actual, obj.ultTrans.tipo_operacion )}
+            />
+        ))}
+      </div>
+    </>
+  )
 }
-
 export default IASugerencias
